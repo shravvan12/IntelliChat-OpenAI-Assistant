cfdbca4be4a9c7d8cee277bdea222318c31c7378
shravvan12 <shravankumar1745590@gmail.com>
2025-09-11 09:26:30 +0530

    added new env variables

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ config();
 const app = express();
 
 //middlewears
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
+app.use(cors({origin: process.env.FRONTEND_URL, credentials: true}));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
